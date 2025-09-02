d2484c1fb601620d3e328fe9ec767bfbe937a88e
Xian7720 <166013378+Xian7720@users.noreply.github.com>
2025-09-03 05:01:19 +0900

    setting custom command

---
+++ src/main/java/me/xian7720/customCommandPlugin/CustomCommandPlugin.java
@@ -0,0 +1,22 @@
+package me.xian7720.customCommandPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public final class CustomCommandPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("Plugin Start");
+        Objects.requireNonNull(getCommand("test")).setExecutor(new command());
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("Plugin end");
+    }
+}
---
+++ src/main/java/me/xian7720/customCommandPlugin/command.java
@@ -0,0 +1,23 @@
+package me.xian7720.customCommandPlugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class command implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+
+        if (commandSender instanceof Player) {
+            Player p = (Player) commandSender;
+            p.sendMessage("성공이다!!!!!?");
+            return true;
+        } else {
+            commandSender.sendMessage("넌 플레이어가 아니다");
+        }
+        return false;
+    }
+}
